2cadba03fb236d484b14007afa2b41da62de9dcb
Huseyin Akten <husakten@gmail.com>
2019-12-16 20:28:14 -0600

    ec2-instance ssh key

--- instance.tf
+++ instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
-  subnet_id  = "subnet-0829ce6b834754cbe"
+  subnet_id  = "subnet-00e6c9565d312b4cc"
   associate_public_ip_address  = "true"
-
+  key_name = "aws_key_pair.deployer.key_name"
 tags = { 
     Name = "HelloWorld" 
   } 
