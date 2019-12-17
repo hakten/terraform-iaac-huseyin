865653aba12ec24314c426d4268a44df309ce298
Huseyin Akten <husakten@gmail.com>
2019-12-16 20:21:51 -0600

    subnet id added

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
- 
+  subnet_id  = "subnet-0829ce6b834754cbe"
 
 tags = { 
     Name = "HelloWorld" 
