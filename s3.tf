bbe8b449aee331c1cd82829d4a7c42872cec82e1
Huseyin Akten <husakten@gmail.com>
2019-12-16 19:20:06 -0600

    enabled s3 versioning

--- s3.tf
+++ s3.tf
@@ -1,8 +1,11 @@
 resource "aws_s3_bucket" "b" { 
-  bucket = "my-tf-test-bucket-huseyin" 
+  bucket = "my-tf-test-bucket" 
   acl    = "private" 
   tags = { 
     Name        = "My bucket" 
     Environment = "Dev" 
+  } 
+  versioning { 
+    enabled = true 
   } 
 } 
