57e338af2f8c67f3477bac3119d14305decd2479
Huseyin Akten <husakten@gmail.com>
2019-12-16 20:23:05 -0600

    public ip added

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
   subnet_id  = "subnet-0829ce6b834754cbe"
+  associate_public_ip_address  = "true"
 
 tags = { 
     Name = "HelloWorld" 
