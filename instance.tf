2f5e5ecc387a5f7c315055a87e19db177e908b68
Huseyin Akten <husakten@gmail.com>
2019-12-21 12:32:19 -0600

    edit

--- instance.tf
+++ instance.tf
@@ -1,26 +1,28 @@
-resource "aws_instance" "web" { 
-  count   = var.count_instance
-  ami           = var.ami 
-  instance_type = var.instance_type
-  associate_public_ip_address  = var.associate_public_ip_address
+resource "aws_instance" "web" {
+  count = var.count_instance
+  ami             = var.ami
+  instance_type   = var.instance_type
+  associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
-  security_groups = ["allow_tls"] 
-
-  provisioner "remote-exec" { 
-   connection { 
-     host        = self.public_ip 
-      type        = "ssh" 
-      user        = var.user 
-      private_key = file(var.ssh_key_location) 
-     } 
-      inline = [ 
-      "sudo yum install -y epel-release",
-      "sudo yum install -y httpd",
-      "systemctl start httpd",
-    ] 
-    } 
-tags = { 
+  security_groups = ["allow_ssh"]
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = var.user
+      private_key = file(var.ssh_key_location)
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd -y ",
+        "systemctl start httpd",
+        ]
+      }
+  lifecycle{
+    prevent_destroy = false
+  }
+  tags = {
     Name = "HelloWorld${count.index +1}"
-  } 
-} 
+  }
+}
 
