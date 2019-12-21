8290cfd191e88fca1d6d4137b6d5270bc91603f3
Huseyin Akten <husakten@gmail.com>
2019-12-21 12:00:22 -0600

    an instance extra removed

--- instance.tf
+++ instance.tf
@@ -11,9 +11,4 @@ tags = {
     Name = "HelloWorld${count.index +1}"
   } 
 } 
-resource "aws_instance" "imported" {
-  ami             = "ami-00068cd7555f543d5" 
-  key_name        = aws_key_pair.deployer.key_name
-  security_groups = ["allow_tls"] 
-  instance_type   = "t2.micro"
-}
+
