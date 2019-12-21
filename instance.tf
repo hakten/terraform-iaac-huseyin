c554ce66ec0cc205f851efa1de056d9ba47e958d
Huseyin Akten <husakten@gmail.com>
2019-12-21 12:26:53 -0600

    provisioner remote exec added

--- instance.tf
+++ instance.tf
@@ -5,7 +5,23 @@ resource "aws_instance" "web" {
   associate_public_ip_address  = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_tls"] 
-  user_data = file("userdata_file")
+
+
+  provisioner "remote-exec" { 
+    connection { 
+      host        = self.public_ip 
+      type        = "ssh" 
+      user        = var.user 
+      private_key = file(var.ssh_key_location) 
+    } 
+    inline = [ 
+      "sudo yum install -y epel-release",
+      "sudo yum install -y httpd",
+      "systemctl start httpd",
+    ] 
+  } 
+
+
 
 tags = { 
     Name = "HelloWorld${count.index +1}"
