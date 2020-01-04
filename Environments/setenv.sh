f2e9fee7ab6cca6f38717459c23c6073d9259d3e
Huseyin Akten <husakten@gmail.com>
2020-01-04 12:51:13 -0600

    removed space

--- Environments/setenv.sh
+++ Environments/setenv.sh
@@ -143,21 +143,13 @@ return 1
 fi 
 
 cat << EOF > "$DIR/backend.tf" 
-
 terraform { 
-
 backend "s3" { 
-
 bucket = "${S3BUCKET}" 
-
 key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}" 
-
 region = "${S3BUCKETREGION}" 
-
   } 
-
 } 
-
-EOF 
+EOF
 
 cat backend.tf 
