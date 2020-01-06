8dd23082c1e8012df89d9a9cedd5435b42e30558
Huseyin Akten <husakten@gmail.com>
2020-01-06 02:29:00 -0600

    eof fixing

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -26,24 +26,24 @@ resource "aws_iam_policy" "example" {
   policy = data.aws_iam_policy_document.example.json 
 } 
 
-resource "aws_iam_policy" "policy" { 
-  name = "test_policy3" 
-  path = "/" 
-  description = "My test policy" 
-  policy = << EOF 
-{ 
-"Version": "2012-10-17", 
-"Statement": [ 
-{ 
-"Sid": "VisualEditor0", 
-"Effect": "Allow", 
-"Action": "ec2:*", 
-"Resource": "*" 
-} 
-] 
-} 
-EOF
-}
+# resource "aws_iam_policy" "policy" { 
+#   name = "test_policy3" 
+#   path = "/" 
+#   description = "My test policy" 
+#   policy = << EOF 
+# { 
+# "Version": "2012-10-17", 
+# "Statement": [ 
+# { 
+# "Sid": "VisualEditor0", 
+# "Effect": "Allow", 
+# "Action": "ec2:*", 
+# "Resource": "*" 
+# } 
+# ] 
+# } 
+# EOF
+# }
 
 resource "aws_iam_role" "test_role" {
 name = "test_role"
