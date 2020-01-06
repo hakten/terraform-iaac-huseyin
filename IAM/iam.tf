731aa2910e86271057424594ed156d1db1ce140e
Huseyin Akten <husakten@gmail.com>
2020-01-06 02:25:47 -0600

    eof edited

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -26,40 +26,40 @@ resource "aws_iam_policy" "example" {
   policy = data.aws_iam_policy_document.example.json 
 } 
 
-# resource "aws_iam_policy" "policy" { 
-#   name = "test_policy3" 
-#   path = "/" 
-#   description = "My test policy" 
-#   policy = <<EOF 
-# { 
-# "Version": "2012-10-17", 
-# "Statement": [ 
-# { 
-# "Sid": "VisualEditor0", 
-# "Effect": "Allow", 
-# "Action": "ec2:*", 
-# "Resource": "*" 
-# } 
-# ] 
-# } 
-# EOF
-# }
+resource "aws_iam_policy" "policy" { 
+  name = "test_policy3" 
+  path = "/" 
+  description = "My test policy" 
+  policy = <<EOF 
+{ 
+"Version": "2012-10-17", 
+"Statement": [ 
+{ 
+"Sid": "VisualEditor0", 
+"Effect": "Allow", 
+"Action": "ec2:*", 
+"Resource": "*" 
+} 
+] 
+} 
+EOF
+}
 
-# resource "aws_iam_role" "test_role" { 
-# name = "test_role" 
-# assume_role_policy = <<EOF 
-# { 
-#   "Version": "2012-10-17", 
-#   "Statement": [ 
-# { 
-#   "Action": "sts:AssumeRole", 
-#   "Principal": { 
-#   "Service": "ec2.amazonaws.com" 
-# }, 
-#   "Effect": "Allow", 
-#   "Sid": "" 
-#     } 
-#   ] 
-# } 
-# EOF 
-# } 
+resource "aws_iam_role" "test_role" {
+name = "test_role"
+assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+{
+  "Action": "sts:AssumeRole",
+  "Principal": {
+  "Service": "ec2.amazonaws.com"
+},
+  "Effect": "Allow",
+  "Sid": ""
+    }
+  ]
+}
+EOF
+}
