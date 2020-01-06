bcfcfc3328f26e159fbdd40c395b9323cbdbc16b
Huseyin Akten <husakten@gmail.com>
2020-01-06 02:38:49 -0600

    eof

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -47,7 +47,7 @@ resource "aws_iam_policy" "example" {
 
 resource "aws_iam_role" "test_role" {
 name = "test_role"
-assume_role_policy = << EOF
+assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
