ca00fd0e89f8b385a20ae48475fb286bfdeff985
Huseyin Akten <husakten@gmail.com>
2020-01-06 02:46:34 -0600

    asdfa

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -34,13 +34,13 @@ resource "aws_iam_policy" "policy" {
   { 
     "Version": "2012-10-17", 
     "Statement": [ 
-    { 
+  { 
     "Sid": "VisualEditor0", 
     "Effect": "Allow", 
     "Action": "ec2:*", 
     "Resource": "*" 
-    } 
-   ] 
+  } 
+  ] 
   } 
 EOF
 }
