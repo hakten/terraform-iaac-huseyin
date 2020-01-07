763d6ce56cf25da1648ca75aa7d64effcc2ce3d1
Huseyin Akten <husakten@gmail.com>
2020-01-07 17:22:46 -0600

    dsa

--- Application/variables.tf
+++ Application/variables.tf
@@ -10,13 +10,10 @@ variable "pub-cidr" {}
 variable "priv-cidr" {}
 variable "enable_nat_gateway" {}
 variable "enable_vpn_gateway" {}
+
 variable "environment" {}
 variable "terraform" {}
 
-
-variable "try" {
-  default = "us-west-1a"
-}
 
 
 
