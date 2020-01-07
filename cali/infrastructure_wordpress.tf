7d68f882424c655f238ab11b9ba9198a32feea43
Huseyin Akten <husakten@gmail.com>
2020-01-07 16:23:51 -0600

    lkjasfd

--- cali/infrastructure_wordpress.tf
+++ cali/infrastructure_wordpress.tf
@@ -4,9 +4,9 @@ module "wordpress" {
   name = "cali"
   cidr = "10.10.0.0/24"
 
-  azs             = ["us-west-1a", "us-west-1b"
-  public_subnets  = ["10.10.1.0/24","10.10.2.0/24"]
-  private_subnets = ["10.10.11.0/24","10.10.12.0/24"]
+  azs             = ["us-west-1a", "us-west-1b"]
+  public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets = ["10.10.11.0/24", "10.10.12.0/24"]
   
   enable_nat_gateway = "false"
   enable_vpn_gateway = "false"
