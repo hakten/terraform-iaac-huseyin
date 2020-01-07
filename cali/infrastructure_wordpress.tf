2cfda172f9eb57d78ddfe08a0002cb7f7f224dec
Huseyin Akten <husakten@gmail.com>
2020-01-07 16:22:44 -0600

    wetr

--- cali/infrastructure_wordpress.tf
+++ cali/infrastructure_wordpress.tf
@@ -5,8 +5,8 @@ module "wordpress" {
   cidr = "10.10.0.0/24"
 
   azs             = ["us-west-1a", "us-west-1b"
-  public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
-  private_subnets = ["10.10.11.0/24", "10.10.12.0/24"]
+  public_subnets  = ["10.10.1.0/24","10.10.2.0/24"]
+  private_subnets = ["10.10.11.0/24","10.10.12.0/24"]
   
   enable_nat_gateway = "false"
   enable_vpn_gateway = "false"
