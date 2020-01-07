5f136365ff74d45bf871a7d7d358721ddcdb81e0
Huseyin Akten <husakten@gmail.com>
2020-01-07 16:26:24 -0600

    ergfsd

--- cali/infrastructure_wordpress.tf
+++ cali/infrastructure_wordpress.tf
@@ -2,7 +2,7 @@ module "wordpress" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "cali"
-  cidr = "10.10.0.0/24"
+  cidr = "10.10.0.0/16"
 
   azs             = ["us-west-1a", "us-west-1b"]
   public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
