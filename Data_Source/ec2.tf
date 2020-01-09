2b26d40ee098b82ae0b4f12a3a8f93e55b8afc8f
Huseyin Akten <husakten@gmail.com>
2020-01-08 18:39:04 -0600

    kljsa

--- Data_Source/ec2.tf
+++ Data_Source/ec2.tf
@@ -1,12 +1,11 @@
-provider "aws" { 
-  region = "us-east-2" 
-} 
+provider "aws" {
+  region = "us-east-2"
+}
 
-data "aws_ami" "ubuntu" { 
-  most_recent = true 
-  owners = ["099720109477"] 
-} 
-
-output "ami" { 
-  value = "${data.aws_ami.ubuntu.id}" 
-} 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+}
+output "ami" {
+  value = "${data.aws_ami.ubuntu.id}"
+}
