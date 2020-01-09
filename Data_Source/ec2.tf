db7d9bc96c16d3a38996821c6cf07f554d5574a8
Huseyin Akten <husakten@gmail.com>
2020-01-08 19:40:35 -0600

    asdf

--- Data_Source/ec2.tf
+++ Data_Source/ec2.tf
@@ -1,24 +1,24 @@
-provider "aws" {
- region = "us-east-2"
-}
+# provider "aws" {
+#  region = "us-east-2"
+# }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"]
-  filter {
-  name = "root-device-type"
-  values = ["ebs"]
-}
-}
-output "ami" {
-  value = "${data.aws_ami.ubuntu.id}"
-}
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners = ["099720109477"]
+#   filter {
+#   name = "root-device-type"
+#   values = ["ebs"]
+# }
+# }
+# output "ami" {
+#   value = "${data.aws_ami.ubuntu.id}"
+# }
 
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "${data.aws_ami.ubuntu.id}"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
