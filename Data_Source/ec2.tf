02465b06491630e8e1aa71ad431b3015497a1519
Huseyin Akten <husakten@gmail.com>
2020-01-08 18:40:38 -0600

    sadf

--- Data_Source/ec2.tf
+++ Data_Source/ec2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-2"
+ region = "us-east-2"
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"]
+  most_recent = true
+  owners = ["099720109477"]
 }
 output "ami" {
-  value = "${data.aws_ami.ubuntu.id}"
+  value = "${data.aws_ami.ubuntu.id}"
 }
