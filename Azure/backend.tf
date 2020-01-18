ef1a869b751a000ee106e61a6e43eebdce780ca6
Huseyin Akten <husakten@gmail.com>
2020-01-18 12:49:40 -0600

    backend added

---
+++ Azure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "example"
+    storage_account_name = "evolvehuseyin"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+  }
+}
